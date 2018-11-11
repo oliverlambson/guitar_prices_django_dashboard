827ac77d951118f62cefa0d8663824d8d3e2a559
Oliver Lambson <oliverlambson@gmail.com>
2018-11-11 13:43:33 +0200

    added view and template to display table of guitars in db table

--- appcompetitors/views.py
+++ appcompetitors/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Guitar
+
+
 def index(request):
-    return HttpResponse("<h1>appcompetitors index</h1>")
+    return render(request, 'appcompetitors/index.html')
+
+
+def guitars(request):
+    guitar_list = Guitar.objects.all()
+    context = {
+        'guitar_list': guitar_list
+    }
+    return render(request, 'appcompetitors/guitars.html', context)
