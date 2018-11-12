3d08b7b1927d769c21cab02cd9ea10fe15aa51d9
Oliver Lambson <oliverlambson@gmail.com>
2018-11-12 21:17:05 +0200

    Added crispy-forms.

--- competitors/settings.py
+++ competitors/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'appcompetitors.apps.AppcompetitorsConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # currently Alpha support (12 Nov 2018)
